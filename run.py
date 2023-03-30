6423adef5d1d125d1edfbca323c5ac1fe33ec1cf
cobaltblu27 <cobaltblu27@gmail.com>
2023-03-30 18:46:31 +0900

    implement tree search to find node type

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 import sys
-from lark import Lark, Transformer, ParseTree
+from typing import Generator
+from lark import Lark, Transformer, ParseTree, exceptions, Token
 
 PROMPT = "DB_2016-19965> "
+query_node_map = {
+    'create_table_query': 'CREATE TABLE',
+    'select_query': 'SELECT',
+    'insert_query': 'INSERT',
+    'drop_query': 'DROP TABLE',
+    'explain_query': 'EXPLAIN',
+    'describe_query': 'DESCRIBE',
+    'show_query': 'SHOW',
+    'delete_query': 'DELETE',
+    'update_query': 'UPDATE',
+}
+EXIT = 'EXIT'
 
 
 class MyTransformer(Transformer):
@@ -12,26 +25,52 @@ class MyTransformer(Transformer):
     def drop_table_query(self, items):
         pass
         # implemet here
-    
+
     def exit(self):
         sys.exit()
 
 
-def get_prompt_input() -> str:
-    str_input: str = input(PROMPT)
-    while (str_input[-1] != ';'):
-        next_line: str = input()
-        str_input += " " + next_line
-    return str_input
+def check_exit(tree: ParseTree) -> bool:
+    matching_nodes = tree.scan_values(lambda n: isinstance(n, Token) and n.type == EXIT)
+    return len(list(matching_nodes)) > 0
+
+
+def get_query_description(tree: ParseTree) -> str:
+    nodes = tree.find_pred(lambda node: node.data in query_node_map.keys())
+    query_node = list(nodes)[0]
+    return query_node.children[0].type
+
+
+def get_prompt_input() -> Generator[str, None, None]:
+    while True:
+        str_input: str = input(PROMPT)
+        while str_input[-1] != ';':
+            next_line: str = input()
+            str_input += " " + next_line
+            str_input = str_input.strip()
+
+        while str_input.find(';') != len(str_input) - 1:
+            split_idx = str_input.find(';')
+            yield str_input[:split_idx + 1]
+            str_input = str_input[split_idx + 1:].strip()
+        yield str_input
 
 
 def prompt(parser: Lark, transformer: Transformer):
-    while(1):
-        query_str = get_prompt_input()
-        output: ParseTree = parser.parse(query_str)
-        # TODO:
-        print("{} {}".format(PROMPT, output.pretty()))
-        transformer.transform(output)
+    input_generator = get_prompt_input()
+    loop = True
+    while loop:
+        try:
+            query_str = next(input_generator)
+            tree: ParseTree = parser.parse(query_str)
+            if check_exit(tree):
+                break
+            query_name = get_query_description(tree)
+            print("{}{} requested".format(PROMPT, query_name))
+            transformer.transform(tree)
+        except exceptions.UnexpectedToken:
+            print("{} Syntax Error".format(PROMPT))
+            input_generator = get_prompt_input()  # reset buffered input
 
 
 if __name__ == "__main__":
@@ -39,13 +78,15 @@ if __name__ == "__main__":
         sql_parser = Lark(file.read(), start="command", lexer="basic")
 
     transformer = MyTransformer()
-    _test = True
-    if (_test):
-        output: ParseTree = sql_parser.parse("select ID from student;")
+    _test = False
+    if _test:
+        # output: ParseTree = sql_parser.parse("select ID from student;")
+        output: ParseTree = sql_parser.parse("desc asdf;")
         print("----------Parsed result----------")
-        print(output.pretty())
+        # print(output.pretty())
+        print(get_query_description(output))
+        # print("{} {}".format(PROMPT, output.children))
 
         transformer.transform(output)
     else:
         prompt(sql_parser, transformer)
-
