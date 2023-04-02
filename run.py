75c48ef51038b3d91c054ece90058091c55c4491
cobaltblu27 <cobaltblu27@gmail.com>
2023-04-02 17:11:46 +0900

    use Transformer class to output prompt

--- run.py
+++ run.py
@@ -1,53 +1,58 @@
 import sys
 import argparse
 from typing import Generator
-from lark import Lark, Transformer, ParseTree, exceptions, Token
+from lark import Lark, Transformer, ParseTree, exceptions
 
-PROMPT = "DB_2016-19965> "
-query_node_map = {
-    'create_table_query': 'CREATE TABLE',
-    'select_query': 'SELECT',
-    'insert_query': 'INSERT',
-    'drop_query': 'DROP TABLE',
-    'explain_query': 'EXPLAIN',
-    'describe_query': 'DESCRIBE',
-    'show_query': 'SHOW',
-    'delete_query': 'DELETE',
-    'update_query': 'UPDATE',
-}
-EXIT = 'EXIT'
+PROMPT = "DB_2016-19965>"
 
+# (테스트용) argument로 query를 받을 수 있게 하였다.
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("-t", "--test", type=str, help="test string")
 
 
-class MyTransformer(Transformer):
+# 명령어에 따라 어떤 명령어가 요청되었는지 출력하도록 한다.
+class SqlTransformer(Transformer):
+    def _print_log(self, query: str):
+        print("{} '{}' requested".format(PROMPT, query))
+
     def create_table_query(self, items):
-        pass
-        # implemet here
+        self._print_log("CREATE TABLE")
+    
+    def select_query(self, items):
+        self._print_log("SELECT")
 
-    def drop_table_query(self, items):
-        pass
-        # implemet here
+    def insert_query(self, items):
+        self._print_log("INSERT")
 
-    def exit(self):
+    def drop_query(self, items):
+        self._print_log("DROP TABLE")
+
+    def explain_query(self, items):
+        self._print_log("EXPLAIN")
+
+    def describe_query(self, items):
+        self._print_log("DESCRIBE")
+
+    def desc_query(self, items):
+        self._print_log("DESC")
+
+    def show_query(self, items):
+        self._print_log("SHOW")
+
+    def delete_query(self, items):
+        self._print_log("DELETE")
+
+    def update_query(self, items):
+        self._print_log("UPDATE")
+
+    def EXIT(self, items):
         sys.exit()
 
 
-def check_exit(tree: ParseTree) -> bool:
-    matching_nodes = tree.scan_values(lambda n: isinstance(n, Token) and n.type == EXIT)
-    return len(list(matching_nodes)) > 0
-
-
-def get_query_description(tree: ParseTree) -> str:
-    nodes = tree.find_pred(lambda node: node.data in query_node_map.keys())
-    query_node = list(nodes)[0]
-    return query_node.children[0].type
-
-
+# semicolon 기준으로 쿼리를 분리하여 parser에 넣을 str을 생성해 주는 generator
 def get_prompt_input() -> Generator[str, None, None]:
     while True:
-        str_input: str = input(PROMPT)
+        str_input: str = input(PROMPT + " ")
         while str_input[-1] != ';':
             next_line: str = input()
             str_input += " " + next_line
@@ -60,28 +65,24 @@ def get_prompt_input() -> Generator[str, None, None]:
         yield str_input
 
 
+# 루프를 돌며 한번에 한 명령어를 parsing한다.
 def prompt(parser: Lark, transformer: Transformer):
     input_generator = get_prompt_input()
-    loop = True
-    while loop:
+    while True:
         try:
             query_str = next(input_generator)
             tree: ParseTree = parser.parse(query_str)
-            if check_exit(tree):
-                break
-            query_name = get_query_description(tree)
-            print("{}{} requested".format(PROMPT, query_name))
             transformer.transform(tree)
         except exceptions.UnexpectedToken:
             print("{} Syntax Error".format(PROMPT))
-            input_generator = get_prompt_input()  # reset buffered input
+            input_generator = get_prompt_input()  # 에러가 날 경우 저장된 다음 명령어들을 버린다.
 
 
 if __name__ == "__main__":
     with open('grammar.lark') as file:
         sql_parser = Lark(file.read(), start="command", lexer="basic")
 
-    transformer = MyTransformer()
+    transformer = SqlTransformer()
 
     args = arg_parser.parse_args()
     test_str = args.test
