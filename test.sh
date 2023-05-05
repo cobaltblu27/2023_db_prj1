789b23cc50a0dfe7bddf999c368968bfaf401817
cobaltblu27 <cobaltblu27@gmail.com>
2023-05-05 21:12:50 +0900

    add test for delete

--- test.sh
+++ test.sh
@@ -1,7 +1,63 @@
 #!/bin/sh
 source venv/bin/activate
 
+rm -rf DB
+
 python run.py -t "
-select st.name from student as st, school
-where created_at < 2008-01-01 and
-(not created_at < "1999-01-01") or st.name = 'Alice';"
+create table school (
+  name char(16),
+  created_at date,
+  primary key (name)
+);
+create table student (
+  id char(10) not null,
+  name char(20) not null,
+  school_name char(16) not null,
+  created_at date,
+  primary key (id),
+  foreign key (school_name) references school(name)
+);
+create table club (
+  id int not null,
+  name char (20),
+  capacity int,
+  primary key (id)
+);
+create table ref (
+  id int,
+  foreign key (id) references club (id)
+);
+create table apply (
+  s_id char (10) not null,
+  l_id int not null,
+  apply_date date,
+  primary key (s_id, l_id),
+  foreign key (s_id) references student (id),
+  foreign key (l_id) references club (id)
+);
+insert into school (name, created_at) values(
+  \"Millennium\",
+  2019-01-01
+);
+insert into school values(
+  \"Trinity\",
+  1980-01-01
+);
+insert into student (id, name, school_name, created_at)
+  values(\"00\", \"Alice\", \"Millennium\", 2020-01-01);
+insert into student (id, name, school_name, created_at)
+  values(\"04\", \"Yuzu\", \"Millennium\", 2000-01-01);
+insert into student (id, name, school_name, created_at)
+  values(\"08\", \"Noah\", \"Millennium\", 2010-01-01);
+insert into student (id, name, school_name, created_at)
+  values(\"michael\", \"Mika\", \"Trinity\", 2005-01-01);
+insert into student (id, name, school_name, created_at)
+  values(\"prr\", \"Hifumi\", \"Trinity\", 2015-01-01);
+
+insert into student (id, name, school_name, created_at)
+  values(\"r\", \"Rabu\", \"Millennium\", 1990-01-01);
+"
+python run.py -t "
+delete from student where (not created_at >= 2000-01-01);
+select * from student;
+"
